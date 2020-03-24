bdd951da560134f962454c2a346ae61a765b83cf
jimyoung <li_jinyang@icloud.com>
2020-03-24 14:41:31 +0800

    No102,409 commit

--- src/cn/jimyoung/leetcode/common/No108_ConvertSortedArrayToBinarySearchTree/ConvertSortedArrayToBinarySearchTree.java
+++ src/cn/jimyoung/leetcode/common/No108_ConvertSortedArrayToBinarySearchTree/ConvertSortedArrayToBinarySearchTree.java
@@ -12,7 +12,19 @@ class TreeNode {
 
 public class ConvertSortedArrayToBinarySearchTree {
 
-    public TreeNode sortedArrayToBST(int[] nums) {
+    private TreeNode fun(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = fun(nums, left, mid - 1);
+        root.right = fun(nums, mid + 1, right);
 
+        return root;
+    }
+
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return fun(nums, 0, nums.length - 1);
     }
 }
